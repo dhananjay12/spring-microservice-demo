c281a297928b97d52fd81c1747567459b01b0ad5
Dhananjay Singh <dhananjay@backbase.com>
2021-05-02 22:26:27 +0200

    temp global filter

--- gateway/src/main/java/com/mynotes/microservices/demo/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/mynotes/microservices/demo/gateway/GatewayApplication.java
@@ -1,9 +1,19 @@
 package com.mynotes.microservices.demo.gateway;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.server.ServerWebExchange;
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -15,5 +25,27 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
+    @Bean
+    CustomGlobalFilter customGlobalFilter() {
+        return new CustomGlobalFilter();
+    }
 
+
+
+}
+
+class CustomGlobalFilter implements GlobalFilter, Ordered {
+
+    private static final Logger log = LoggerFactory.getLogger(CustomGlobalFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("Custom global filter");
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
 }
