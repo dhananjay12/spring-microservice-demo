f9f8e39111948b3607730e56f2220c01bf89b83a
Dhananjay Singh <dhananjay@backbase.com>
2021-10-09 17:13:57 +0200

    logging

--- service-one/src/main/java/com/mynotes/microservices/demo/serviceone/LoggingInterceptor.java
+++ service-one/src/main/java/com/mynotes/microservices/demo/serviceone/LoggingInterceptor.java
@@ -17,13 +17,24 @@ public class LoggingInterceptor implements ClientHttpRequestInterceptor {
     @Override
     public ClientHttpResponse intercept(
             HttpRequest req, byte[] reqBody, ClientHttpRequestExecution ex) throws IOException {
-        log.debug("Request body: {}", new String(reqBody, StandardCharsets.UTF_8));
+
+        log.debug("===========================request begin================================================");
+        log.debug("URI         : {}", req.getURI());
+        log.debug("Method      : {}", req.getMethod());
+        log.debug("Headers     : {}", req.getHeaders() );
+        log.debug("Request body: {}", new String(reqBody, "UTF-8"));
+        log.debug("==========================request end================================================");
         ClientHttpResponse response = ex.execute(req, reqBody);
         InputStreamReader isr = new InputStreamReader(
                 response.getBody(), StandardCharsets.UTF_8);
         String body = new BufferedReader(isr).lines()
                 .collect(Collectors.joining("\n"));
+        log.info("============================response begin==========================================");
+        log.debug("Status code  : {}", response.getStatusCode());
+        log.debug("Status text  : {}", response.getStatusText());
+        log.debug("Headers      : {}", response.getHeaders());
         log.debug("Response body: {}", body);
+        log.info("=======================response end=================================================");
         return response;
     }
 }
