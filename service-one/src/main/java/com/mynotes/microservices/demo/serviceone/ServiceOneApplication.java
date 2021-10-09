7e023a989f8381a26e2dcadc5ecc68c1efd2a571
Dhananjay Singh <dhananjay@backbase.com>
2021-10-09 16:21:19 +0200

    version update

--- service-one/src/main/java/com/mynotes/microservices/demo/serviceone/ServiceOneApplication.java
+++ service-one/src/main/java/com/mynotes/microservices/demo/serviceone/ServiceOneApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -19,7 +22,11 @@ public class ServiceOneApplication {
 	@LoadBalanced
 	@Bean
 	RestTemplate restTemplate() {
-		return new RestTemplate();
+
+		ClientHttpRequestFactory factory =
+				new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory());
+
+		return new RestTemplate(factory);
 	}
 
 }
