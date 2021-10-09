c4d7fd24ccffd8c2213511ce031c6620fc552b28
Dhananjay Singh <dhananjay@backbase.com>
2021-10-09 16:31:38 +0200

    version update

--- service-one/src/main/java/com/mynotes/microservices/demo/serviceone/ServiceOneApplication.java
+++ service-one/src/main/java/com/mynotes/microservices/demo/serviceone/ServiceOneApplication.java
@@ -1,5 +1,9 @@
 package com.mynotes.microservices.demo.serviceone;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -7,12 +11,17 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ServiceOneApplication {
+
+	static Logger LOGGER = LoggerFactory.getLogger(ServiceOneApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceOneApplication.class, args);
@@ -22,11 +31,23 @@ public class ServiceOneApplication {
 	@LoadBalanced
 	@Bean
 	RestTemplate restTemplate() {
+		RestTemplate restTemplate = null;
+		if (LOGGER.isDebugEnabled()) {
+			ClientHttpRequestFactory factory
+					= new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory());
+			List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
+			if (CollectionUtils.isEmpty(interceptors)) {
+				interceptors = new ArrayList<>();
+			}
+			interceptors.add(new LoggingInterceptor());
+			restTemplate.setInterceptors(interceptors);
+			restTemplate = new RestTemplate(factory);
 
-		ClientHttpRequestFactory factory =
-				new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory());
+		} else {
+			restTemplate = new RestTemplate();
+		}
 
-		return new RestTemplate(factory);
+		return restTemplate;
 	}
 
 }
